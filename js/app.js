1978a0e35db3845ebb848684952761da64cdc799
Luciano Signorelli <luciano.signorelli@lmvsoftware.com>
2023-04-19 15:03:29 -0300

    add js

---
+++ js/app.js
@@ -0,0 +1,24 @@
+console.log("Hola a todos");
+console.log(2 + 2);
+console.warn("Warning, cuidado");
+
+
+// variables
+let edad = 23;
+
+let nombre;
+nombre = "Luciano"
+
+let apellido = "Signorelli";
+
+
+console.error("Error, fatal error");
+console.log(apellido);
+console.log(nombre);
+console.log(edad);
+console.log("Modificamos la edad")
+edad = "Perez";
+console.log(edad);
+console.log("Error, fatal error");
+console.info("Error, fatal error");
+console.warn("Error, fatal error");
