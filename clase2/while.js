d2f445943af702ad03a7d1cc466fcc9ac3932f82
Luciano Signorelli <luciano.signorelli@lmvsoftware.com>
2023-04-24 14:59:40 -0300

    JS
    Clase 2

---
+++ clase2/while.js
@@ -0,0 +1,29 @@
+//Bucle While
+
+// let i = 0;
+// while(i < 5){
+//     debugger;
+//     console.log('Valor de i: ', i);
+//     if(i == 3)
+//         console.log('Llegamos al 3')
+//     i++;
+// }
+
+// let nota = parseInt(prompt('Ingrese su nota'));
+// while( nota != -1){
+//     nota = parseInt(prompt('Ingrese su nota'));
+//     console.log(nota);
+// }
+
+
+
+// BUCLE FOR
+
+//for (inicializacion, condicion; incremento)
+
+for(let i = 0; i < 10; i++){
+    console.log('valor de i++: ',  i)
+}
+for(let j = 0; j < 10; ++j){
+    console.log('valor de ++j: ', j);
+}
