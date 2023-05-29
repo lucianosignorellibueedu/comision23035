e1a0f7a7fa6a164ac61e22c0366e576a6c4ddda1
Luciano Signorelli <luciano.signorelli@lmvsoftware.com>
2023-05-29 15:37:27 -0300

    Create 01.py

---
+++ Python/01.py
@@ -0,0 +1,14 @@
+a = 5
+b = 5
+if a == b:
+    print('Son iguales')
+    if a == 5:
+        print('valor de A: ', a)
+if a != b:
+    # TODO: Ver que pasa si son distintos
+    pass
+
+print(type(a))
+a = 'Hola Mundo'
+print(a)
+print(type(a))
